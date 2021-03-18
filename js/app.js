a7fe7918dbd84ee5c6cf010d0d51c886e08389b4
poris <sw130nam@gmail.com>
2021-03-18 22:24:03 +0800

    update

--- js/app.js
+++ js/app.js
@@ -10,21 +10,13 @@
 
     $(document).ready(function () {
         disableBodyScroll();
-        videoWrapper();
-        videoSoundToggle();
         smoothScroll();
         backgroundColor();
-        jsCounter();
-        masonaryFilter();
-        testimonialCarousel();
-        serviceCarousel();
-        partnerCarousel();
-        workCarousel();
+        masonaryFilter();        
     })
 
     $(window).on('load', function () {
         isotopeInit();
-        magnificPopup();
     })
 
     // AOS init
@@ -81,33 +73,6 @@
         })
     }
 
-    // Inline videoWrapper Init
-    function videoWrapper() {
-        $('.popup-video').on('mouseenter', function() {
-            $('.video-wrapper').addClass('open-video')
-        })
-        $('.popup-video').on('mouseleave', function() {
-            $('.video-wrapper').removeClass('open-video')
-        })
-    }
-
-    // videoSoundToggle Init
-    function videoSoundToggle() {
-        $('.html5-video video').prop('muted', true)
-        $('.sound-control svg').on('click', function (){
-            if( $('.html5-video video').prop('muted') ) {
-                $('.html5-video video').prop('muted', false)
-            } else {
-            $('.html5-video video').prop('muted', true)
-            }
-        });
-        $('.volume-up').hide()
-        $('.sound-control').on('click', function(){
-            $('.volume-up').toggle()
-            $('.volume-mute').toggle()
-        })
-    }
-
     // Smooth-Scroll init
     function smoothScroll() {
         var scrollLink = $('.scroll-to-anim');
@@ -135,19 +100,6 @@
         })
     }
 
-    // jsCounter init
-    function jsCounter() {
-        $('.js-counter').appear(function() {
-            var count = $(this);
-            count.countTo({
-                from: 0,
-                to: count.html(),
-                speed: 1200,
-                refreshInterval: 60
-            })
-        })
-    }
-
     // mesonary-filter init
     function masonaryFilter() {
         var $masonaryFilter = $('.mesonary-filter');
@@ -166,82 +118,6 @@
         var $mesonaryInit = $('.mesonaryInit').isotope();
         $mesonaryInit.imagesLoaded().progress( function() {
             $mesonaryInit.isotope('layout');
-        })
-    }
-
-    // magnificPopup Init
-    function magnificPopup() {
-        $('.zoom-image').magnificPopup({
-            delegate: '.zoom-image-item',
-            type: 'image',
-            removalDelay: 200,
-            callbacks: {
-                beforeOpen: function() {
-                    this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-                    this.st.mainClass = this.st.el.attr('data-effect');
-                }
-            },
-            gallery: {
-                enabled: !0
-            },
-            image: {
-                verticalFit: !0
-            },
-            closeOnContentClick: true,
-            midClick: true 
-        })
-        if ($('.popup-video').length > 0) {
-            $('.popup-video').magnificPopup({
-                type: 'iframe',
-                mainClass: 'mfp-fade',
-                removalDelay: 200,
-                preloader: true,
-                fixedContentPos: true,
-            })
-        }
-    }
-
-    // TestimonialCarousel Init
-    function testimonialCarousel() {
-        $('.testimonial-carousel').flickity({
-            cellAlign: 'center',
-            prevNextButtons: false,
-            contain: true
-        })
-    }
-
-    // serviceCarousel Init
-    function serviceCarousel() {
-        $('.service-carousel').flickity({
-            cellAlign: 'left',
-            prevNextButtons: false,
-            pageDots: false,
-            freeScroll: true,
-            contain: true
-        })
-    }
-
-    // partnerCarousel Init
-    function partnerCarousel() {
-        $('.partner-carousel').flickity({
-            cellAlign: 'left',
-            prevNextButtons: false,
-            pageDots: false,
-            freeScroll: true,
-            contain: true,
-            autoPlay: 3000,
-            wrapAround: true,
-            pauseAutoPlayOnHover: false
-        })
-    }
-
-    // workCarousel Init
-    function workCarousel() {
-        $('.work-carousel').flickity({
-            cellAlign: 'left',
-            prevNextButtons: true,
-            pageDots: false,
-            wrapAround: true
         })
     }
 
